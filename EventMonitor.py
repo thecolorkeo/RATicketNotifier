51e4ad69c3cd0fcc915c0ffd2938660e70b6633a
keoc <keo.chan@doubleverify.com>
2019-10-20 01:42:42 -0400

    README and cleaning code

--- EventMonitor.py
+++ EventMonitor.py
@@ -24,12 +24,10 @@ def get_page(url: str):
 def split_by_tag(page: str, tag: str) -> str:
     """
     :param page: full page text in string format
-    :param tag: the class parameter in an html tag, preceded by a dot.
-    Not sure I explained this correctly, so here's an example
+    :param tag: the 'class' parameter in an html tag, preceded by a dot. Example:
     In this html tag: <h1 id="MembersFavouriteCount" class="favCount">551</h1>
     the search term is ".favCount"
     """
-
     raw = bs4.BeautifulSoup(page, 'html.parser')
     ticket_subset = raw.select(tag)[0].decode_contents()  # [3413:]
     return ticket_subset
@@ -50,12 +48,16 @@ def send_email(body):
     conn.quit()
     print(f'Sent notification e-mails for the following recipients: {toAddress}')
 
-def poll_page_once(page):
-    subset = split_by_tag(page, '.ticket-list-item')
-    freq = subset.count('closed')
 
-    if freq < 4:
-        send_email(f'''Subject: Tickets available ({datetime.now().strftime("%I:%M:%S %p")})!\n\nNumber available: {4 - freq}\n\nRaw text: {subset.encode('utf-8')}''')
+def parse_page(page):
+    subset = split_by_tag(page, '.ticket-list-item')
+    num_ticket_releases = subset.count("release")
+    current_closed_releases = subset.count('closed')
+
+    if current_closed_releases < num_ticket_releases:
+        send_email(f'''Subject: Tickets available ({datetime.now().strftime("%I:%M:%S %p")})!\n\n
+        Number available: {4 - current_closed_releases}\n\n
+        Raw text: {subset.encode('utf-8')}''')
         print("Tickets available! Sent email")
     else:
         print("No available tickets, did not send email")
@@ -63,11 +65,12 @@ def poll_page_once(page):
 
 if __name__ == '__main__':
     starttime = time.time()
-    delay = 60.0
+    delay = 30.0
 
     page = get_page(os.environ['page_url']).text
+    parse_page(page)
 
     while True:
         print(f"polling {datetime.now()}. Current number of members attending: {count_attending(page)}")
-        poll_page_once(page)
+        parse_page(page)
         time.sleep(delay - ((time.time() - starttime) % delay))
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-### RATicketNotifier
-Web scraping application that sends me an email when sold out tickets become reavailable
+# RATicketNotifier
+Web scraping application that sends an email when sold out tickets become reavailable
 
-# How it works
+###Motivation
+When an event on Resident Advisor sells out, they offer an official resale channel through their website. When someone gives up their ticket, the button on their site switches from sold out to available, but there's otherwise no notification that more tickets have become available. Often many hours or days go by between ticket resales. To avoid refreshing the page regularly and hoping to get lucky, I opted to write this script to scrape the page every minute and email me if a ticket becomes available.
+
+Their website offers another useful feature that lists the number of members attending a given event. I noticed that when tickets are resold, the member count doesn't go down - so this can be used as a soft check on whether this application is working properly. So if the number of members attending increases, you know that someone sold a ticket. Though, if someone who already has a ticket buys another, the member count does not increase. 
+
+### How it works
 RA uses an html tag of "closed" to represent sold out tickets. Note, there are often multiple ticket releases on the same event. My app parses the page and extracts the number of "closed" strings in the raw html. If this number drops (ie., one of the ticket releases becomes available) then it sends an email to let you know. The page url and email settings are controlled through environment variables. Made this primarily for my own use.
+
+### How to use
+Add your email_address, password (recommended to use appkey instead) and the url of the event to your env variables. Then run scraper.py. Feel free to customize the email you send to yourself.
